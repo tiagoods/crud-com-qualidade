abd96e221368fc9be81c8df7f5077b6bcc78a31c
Tiago Oliveira dos Santos <tiago.sie90@gmail.com>
2023-10-11 19:02:59 -0300

    Aula 2.1 Parte 2

--- core/crud.ts
+++ core/crud.ts
@@ -1,10 +1,10 @@
-const fs = require("fs");
+import fs from "fs";
 
 const DB_FILE_PATH = "./core/db";
 
 console.log("[CRUD]");
 
-function create(content) {
+function create(content: string) {
   fs.writeFileSync(DB_FILE_PATH, content);
   return content;
 }
