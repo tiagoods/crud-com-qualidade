2a681ae46d22e2e3ca39e1f4dbbe26f96aeb8dc5
Tiago Oliveira dos Santos <tiago.sie90@gmail.com>
2023-10-11 18:53:32 -0300

    Aula 2.1 Parte 1

--- core/crud.js
+++ core/crud.js
@@ -1,1 +1,14 @@
+const fs = require("fs");
+
+const DB_FILE_PATH = "./core/db";
+
 console.log("[CRUD]");
+
+function create(content) {
+  fs.writeFileSync(DB_FILE_PATH, content);
+  return content;
+}
+
+// [SIMULATION]
+
+console.log(create("Hoje eu preciso gravar aulas!"));
